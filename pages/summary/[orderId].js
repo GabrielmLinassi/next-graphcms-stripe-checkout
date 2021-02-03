6cd2a2d1748097d9cda5ad3f3e22212d0d8933cc
Gabriel <gabrielm.linassi@gmail.com>
2021-02-02 21:15:31 -0300

    Style summary page

--- pages/summary/[orderId].js
+++ pages/summary/[orderId].js
@@ -1,43 +1,51 @@
 import React from "react";
 import { gql, GraphQLClient } from "graphql-request";
 import { formatPrice } from "../../components/helper";
+import Layout from "components/Layout";
+import BackBtn from "components/BackBtn";
 
 const graphcms = new GraphQLClient(process.env.GRAPHCMS_API);
 
-export default function SummaryPage({ orders }) {
+export default function SummaryPage({ order }) {
   {
-    return orders.map((order) => {
-      return (
-        <div key={order.id}>
-          <div>Total Order: {formatPrice(order.total)}</div>
-          <div>
-            {order.orderItems.map((orderItem) => (
-              <div key={orderItem.id}>
-                <div>Quantity: {orderItem.quantity}</div>
-                <div>Price: {formatPrice(orderItem.price)}</div>
-                <div>Product: {orderItem.product.name}</div>
-                <img
-                  src={orderItem.product.images[0].url}
-                  alt={orderItem.product.images[0].fileName}
-                  width={orderItem.product.images[0].width}
-                  height={orderItem.product.images[0].height}
-                />
+    return (
+      <Layout>
+        <BackBtn title="Back home" />
+        <h1 className="text-2xl font-bold mt-3">Order Summary</h1>
+        <ul className="bg-white p-5 rounded-md shadow-md mt-3">
+          {order.orderItems.map((orderItem) => (
+            <li key={orderItem.id} className="flex gap-5">
+              <img
+                src={orderItem.product.images[0].url}
+                alt={orderItem.product.images[0].fileName}
+                width={100}
+                height={100}
+              />
+              <div>
+                <div>
+                  {orderItem.quantity} x {orderItem.product.name}
+                </div>
+                <div className="mt-2">{formatPrice(orderItem.price)}</div>
               </div>
-            ))}
-          </div>
-        </div>
-      );
-    });
+            </li>
+          ))}
+          <li className="border-t-2 pt-3 mt-5 text-right">
+            <span className="font-bold">Total Order:</span>{" "}
+            {formatPrice(order.total)}
+          </li>
+        </ul>
+      </Layout>
+    );
   }
 }
 
 export async function getServerSideProps(context) {
   const { orderId } = context.query;
 
-  const { orders } = await graphcms.request(
+  const { order } = await graphcms.request(
     gql`
       query OrderFullfilment($stripeCheckoutId: String!) {
-        orders(where: { stripeCheckoutId: $stripeCheckoutId }) {
+        order(where: { stripeCheckoutId: $stripeCheckoutId }) {
           id
           total
           orderItems {
@@ -64,7 +72,7 @@ export async function getServerSideProps(context) {
 
   return {
     props: {
-      orders,
+      order,
     },
   };
 }
