f1d9a10272866ea26769de9e8adb1668da9de4cb
Gabriel <gabrielm.linassi@gmail.com>
2021-02-06 20:12:33 -0300

    test

--- pages/api/utils/auth0.js
+++ pages/api/utils/auth0.js
@@ -1,15 +1,15 @@
-// import { initAuth0 } from "@auth0/nextjs-auth0";
+import { initAuth0 } from "@auth0/nextjs-auth0";
 
-// export default initAuth0({
-//   domain: "dev-qgvsfan0.us.auth0.com",
-//   clientId: "pMfHceXVY3qKMtWV8qIaA8LAtmyXcci9",
-//   clientSecret:
-//     "zVxWnWlF_N6nMmZ-yIaJU7NTb3G9poPECKtEXx3y2_bjwmsvKGThdux3x-VGCTp5",
-//   scope: "openid profile",
-//   redirectUri: "https://next-graphcms-stripe-checkout.vercel.app/api/callback",
-//   postLogoutRedirectUri: "https://next-graphcms-stripe-checkout.vercel.app",
-//   session: {
-//     cookieSecret:
-//       "hsahhsadsadlkewrwerldlsfdf-03-4323-249tkiretkgbp-dsfsdf;sasapppppwq",
-//   },
-// });
+export default initAuth0({
+  domain: "dev-qgvsfan0.us.auth0.com",
+  clientId: "pMfHceXVY3qKMtWV8qIaA8LAtmyXcci9",
+  clientSecret:
+    "zVxWnWlF_N6nMmZ-yIaJU7NTb3G9poPECKtEXx3y2_bjwmsvKGThdux3x-VGCTp5",
+  scope: "openid profile",
+  redirectUri: "https://next-graphcms-stripe-checkout.vercel.app/api/callback",
+  postLogoutRedirectUri: "https://next-graphcms-stripe-checkout.vercel.app",
+  session: {
+    cookieSecret:
+      "hsahhsadsadlkewrwerldlsfdf-03-4323-249tkiretkgbp-dsfsdf;sasapppppwq",
+  },
+});
