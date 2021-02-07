a9f528aee195e4e886c4c7374bb17a6b65fd30ad
Gabriel <gabrielm.linassi@gmail.com>
2021-02-06 21:22:19 -0300

    test

--- pages/api/callback.js
+++ pages/api/callback.js
@@ -1,21 +1,8 @@
-import { initAuth0 } from "@auth0/nextjs-auth0";
+import auth0 from "./utils/auth0";
 
 export default async function callback(req, res) {
   try {
-    await initAuth0({
-      domain: "dev-qgvsfan0.us.auth0.com",
-      clientId: "pMfHceXVY3qKMtWV8qIaA8LAtmyXcci9",
-      clientSecret:
-        "zVxWnWlF_N6nMmZ-yIaJU7NTb3G9poPECKtEXx3y2_bjwmsvKGThdux3x-VGCTp5",
-      scope: "openid profile",
-      redirectUri:
-        "https://next-graphcms-stripe-checkout.vercel.app/api/callback",
-      postLogoutRedirectUri: "https://next-graphcms-stripe-checkout.vercel.app",
-      session: {
-        cookieSecret:
-          "hsahhsadsadlkewrwerldlsfdf-03-4323-249tkiretkgbp-dsfsdf;sasapppppwq",
-      },
-    }).handleCallback(req, res, { redirectTo: "/" });
+    await auth0.handleCallback(req, res, { redirectTo: "/" });
   } catch (error) {
     console.error(error);
     res.status(error.status || 400).end(error.message);
--- pages/api/login.js
+++ pages/api/login.js
@@ -1,21 +1,8 @@
-import { initAuth0 } from "@auth0/nextjs-auth0";
+import auth0 from "./utils/auth0";
 
 export default async function login(req, res) {
   try {
-    await initAuth0({
-      domain: "dev-qgvsfan0.us.auth0.com",
-      clientId: "pMfHceXVY3qKMtWV8qIaA8LAtmyXcci9",
-      clientSecret:
-        "zVxWnWlF_N6nMmZ-yIaJU7NTb3G9poPECKtEXx3y2_bjwmsvKGThdux3x-VGCTp5",
-      scope: "openid profile",
-      redirectUri:
-        "https://next-graphcms-stripe-checkout.vercel.app/api/callback",
-      postLogoutRedirectUri: "https://next-graphcms-stripe-checkout.vercel.app",
-      session: {
-        cookieSecret:
-          "hsahhsadsadlkewrwerldlsfdf-03-4323-249tkiretkgbp-dsfsdf;sasapppppwq",
-      },
-    }).handleLogin(req, res);
+    await auth0.handleLogin(req, res);
     res.status(200);
     res.send({ message: "login working" });
   } catch (error) {
