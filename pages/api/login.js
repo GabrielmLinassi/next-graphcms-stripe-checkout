311ec7809f8eeb295a736d1b73a3b00527e34cd2
Gabriel <gabrielm.linassi@gmail.com>
2021-02-06 21:26:45 -0300

    test

--- pages/api/login.js
+++ pages/api/login.js
@@ -3,11 +3,8 @@ import auth0 from "./utils/auth0";
 export default async function login(req, res) {
   try {
     await auth0.handleLogin(req, res);
-    res.status(200);
-    res.send({ message: "login working" });
   } catch (error) {
     console.error(error);
-    res.status(error.status || 400);
-    res.send("error ===>", error.message);
+    res.status(error.status || 400).end(error.message);
   }
 }
