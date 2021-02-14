b1b1710b562c1541f0b172c4d291e43f8787788b
Gabriel <gabrielm.linassi@gmail.com>
2021-02-13 23:16:16 -0300

    Fix error My Orders

--- pages/orders.js
+++ pages/orders.js
@@ -8,34 +8,38 @@ export default function orders({ orders }) {
   return (
     <Layout>
       <h1 className="text-2xl font-bold mt-3">My Orders</h1>
-      <ul>
-        {orders.map((order) => {
-          return (
-            <li
-              key={order.id}
-              className="bg-white p-5 rounded-md shadow-md mt-3"
-            >
-              <ul>
-                {order.orderItems.map((orderItem) => {
-                  return (
-                    <li key={orderItem.id} className="mb-5">
-                      <div>{orderItem.product.name}</div>
-                      {orderItem.quantity} x {formatPrice(orderItem.price)}
-                    </li>
-                  );
-                })}
-              </ul>
-              <div>Total: {formatPrice(order.total)}</div>
-            </li>
-          );
-        })}
-      </ul>
+      {orders.length ? (
+        <ul>
+          {orders.map((order) => {
+            return (
+              <li
+                key={order.id}
+                className="bg-white p-5 rounded-md shadow-md mt-3"
+              >
+                <ul>
+                  {order.orderItems.map((orderItem) => {
+                    return (
+                      <li key={orderItem.id} className="mb-5">
+                        <div>{orderItem.product.name}</div>
+                        {orderItem.quantity} x {formatPrice(orderItem.price)}
+                      </li>
+                    );
+                  })}
+                </ul>
+                <div>Total: {formatPrice(order.total)}</div>
+              </li>
+            );
+          })}
+        </ul>
+      ) : (
+        <div className="bg-white p-5 rounded-md shadow-md mt-3">Not found</div>
+      )}
     </Layout>
   );
 }
 
 export async function getServerSideProps(context) {
-  const { user } = await auth0.getSession(context.req);
+  const { user } = (await auth0.getSession(context.req)) || {};
 
   const { orders } = await graphcms.request(
     gql`
@@ -55,7 +59,7 @@ export async function getServerSideProps(context) {
       }
     `,
     {
-      customerId: user.sub,
+      customerId: user?.sub || "",
     }
   );
 
