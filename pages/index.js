c0d476aae05ee40a1b809e63ce8b285bdd30f178
Gabriel <gabrielm.linassi@gmail.com>
2021-03-16 17:56:04 -0300

    Add side space for filtering

--- pages/index.js
+++ pages/index.js
@@ -5,55 +5,30 @@ import Layout from "../components/Layout";
 import { formatPrice } from "../components/helper";
 import { InstantSearch, SearchBox, Hits } from "react-instantsearch-dom";
 const algoliasearch = require("algoliasearch");
-// import "instantsearch.css/themes/algolia.css";
 import "instantsearch.css/themes/reset.css";
 import styled from "styled-components";
 
 const client = algoliasearch("MRLYG735R2", "553f555a65bcc73f82e29ffdc73e503b");
 const graphcms = new GraphQLClient(process.env.GRAPHCMS_API);
 
-export default function Home({ products }) {
+export default function Home() {
   return (
     <Layout title="NextJS GraphCMS Stripe Checkout">
       <InstantSearch searchClient={client} indexName="products">
-        <StyledSearchBox />
-        <Hits hitComponent={Hit} />
-        {/* <Products products={products} /> */}
-        {/* </ul> */}
+        <Split>
+          <Side>left</Side>
+          <Main>
+            <StyledSearchBox />
+            <Hits hitComponent={Hit} />
+          </Main>
+        </Split>
       </InstantSearch>
     </Layout>
   );
 }
 
-const Products = ({ products }) => {
-  return products.map(({ id, name, price, slug, images }) => {
-    return (
-      <li
-        key={id}
-        className="flex flex-col items-center gap-5 bg-white rounded-md shadow-md p-5"
-      >
-        {images.length > 0 && (
-          <Image
-            src={images[0].url}
-            width={500} // could be images[0].width however value is 1000
-            height={500} // could be images[0].height however value is 1000
-            alt={images[0].fileName}
-          />
-        )}
-        <div>
-          <Link key={id} href={`/products/${slug}`}>
-            <a>{name}</a>
-          </Link>
-          <div className="font-bold mt-3">{formatPrice(price)}</div>
-        </div>
-      </li>
-    );
-  });
-};
-
 const Hit = ({ hit: { objectID, name, price, slug, images } }) => {
   return (
-    // <div className="flex flex-col items-center gap-5 bg-white rounded-md shadow-md p-5">
     <Link key={objectID} href={`/products/${slug}`}>
       <a>
         {images.length && <Image src={images[0]} width={500} height={500} />}
@@ -97,6 +72,18 @@ export async function getStaticProps() {
     },
   };
 }
+
+const Main = styled.div``;
+
+const Side = styled.div`
+  background-color: red;
+  padding: 0 5rem;
+`;
+
+const Split = styled.div`
+  display: flex;
+  gap: 0.5rem;
+`;
 
 const StyledSearchBox = styled(SearchBox)`
   .ais-SearchBox-form {
