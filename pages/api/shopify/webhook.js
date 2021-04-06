6f231b727505d040730222925ddb4d60342b1ef2
Gabriel <gabrielm.linassi@gmail.com>
2021-04-06 19:48:08 -0300

    fix

--- pages/api/shopify/webhook.js
+++ pages/api/shopify/webhook.js
@@ -3,12 +3,15 @@ const client = algoliasearch("MRLYG735R2", "553f555a65bcc73f82e29ffdc73e503b");
 const index = client.initIndex("products-shopify");
 
 export default async function AddtoAlgolia(req, res) {
-  console.log({ product: JSON.stringify(req.body) });
+  //console.log({ product: JSON.stringify(req.body) });
 
   try {
-    // const { id, title, body_html, handle, status, variants, images } = req.body;
-    // const { price, inventory_quantity } = variants[0];
-    /**
+    const { id, title, body_html, handle, status, variants, images, metafields } = req.body;
+    const { price, inventory_quantity } = variants[0];
+
+    const { value: comments } = metafields.find((metafield) => metafield.key === "comments");
+    const { value: stars } = metafields.find((metafield) => metafield.key === "rating");
+
     const product = {
       objectID: id,
       title,
@@ -17,13 +20,18 @@ export default async function AddtoAlgolia(req, res) {
       status,
       price,
       inventory_quantity,
+      rating: Math.floor(stars),
+      stars,
+      comments,
+      image: images[0].src,
       images: images.map(({ id, src }) => ({
         objectID: id,
         src,
       })),
     };
 
-    await index.saveObject(product); */
+    console.log({ product });
+    await index.saveObject(product);
     res.send(201);
   } catch (err) {
     console.log(err);
