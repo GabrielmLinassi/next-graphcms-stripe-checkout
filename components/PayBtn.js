48b184c849cb53a29f608903f67eb819336d2daa
Gabriel <gabrielm.linassi@gmail.com>
2021-04-23 11:34:33 -0300

    Replace `Buy` button on the product by the `go to checkout`

--- components/PayBtn.js
+++ components/PayBtn.js
@@ -1,48 +1,24 @@
-import { useContext } from "react";
-import { loadStripe } from "@stripe/stripe-js";
+import { useRouter } from "next/router";
+import { useAddCart } from "components/AddCartButton";
+import tw from "twin.macro";
 
-import { useAuth } from "contexts/auth";
-import { CartContext } from "contexts/CartProvider";
-import { clearCart } from "reducers/CartReducer";
+const PayButton = ({ variantId, quantity }) => {
+  const { addToCartHandler } = useAddCart();
+  const router = useRouter();
 
-const stripePromise = loadStripe(process.env.STRIPE_PUBLIC_KEY);
-
-const PayButton = ({ products, full = false }) => {
-  const { dispatch } = useContext(CartContext);
-  const { user } = useAuth();
-
-  const handleClick = async () => {
-    if (!user) {
-      alert("User must be logged in to buy.");
-      return;
-    }
-    const stripe = await stripePromise;
-
-    const session = await fetch("/api/create-checkout-session", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        products,
-      }),
-    }).then((res) => res.json());
-
-    await stripe.redirectToCheckout({
-      sessionId: session.id,
-    });
+  const handleClick = () => {
+    addToCartHandler(variantId, quantity);
+    router.push("/checkout");
   };
 
   return (
     <button
       onClick={handleClick}
-      className={`inline-block bg-blue-500 text-white rounded-md px-4 py-2 hover:bg-blue-600 ${
-        full && "w-full"
-      }`}
+      tw="inline-block bg-blue-500 text-white rounded-md px-4 py-2 hover:bg-blue-600 w-full"
     >
       Buy
     </button>
   );
 };
 
-export { PayButton };
+export default PayButton;
--- pages/products/[slug].js
+++ pages/products/[slug].js
@@ -1,17 +1,14 @@
 import { useState } from "react";
 import { initializeApollo } from "libs/apollo";
-import { useRouter } from "next/router";
-import Link from "next/link";
 
 import { formatPrice } from "components/helper";
-import { PayButton } from "components/PayBtn";
+import PayButton from "components/PayBtn";
 import Layout from "components/Layout";
 import BackBtn from "components/BackBtn";
 import Carousel from "components/carousel/Carousel";
 import AddCart from "components/AddCartButton";
 import Amount from "components/AmountButton";
 import { AllProducts, ProductByHandle } from "queries/product";
-import { useAddCart } from "components/AddCartButton";
 
 /* --- --- --- */
 
@@ -51,9 +48,6 @@ const ProductPage = ({ product }) => {
   const { id, handle: slug, title: name, description, images, variants } = product;
   const { id: variantId, price } = variants.edges[0].node;
 
-  const router = useRouter();
-  const { addToCartHandler } = useAddCart();
-
   const allImages = images.edges.map((edge) => ({
     id: edge.node.id,
     src: edge.node.originalSrc,
@@ -79,15 +73,7 @@ const ProductPage = ({ product }) => {
               <Amount quantity={quantity} setQuantity={setQuantity} />
             </div>
             <div className="mt-5">
-              <button
-                onClick={() => {
-                  addToCartHandler(variantId, quantity);
-                  router.push("/checkout");
-                }}
-              >
-                Go to Checkout
-              </button>
-              <PayButton products={[{ id, quantity }]} full />
+              <PayButton variantId={variantId} quantity={quantity} />
             </div>
             <AddCart variantId={variantId} quantity={quantity} />
           </div>
