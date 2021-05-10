fdf0def705ac1c576c66ad3fcbe17ddbc66ba944
Gabriel <gabrielm.linassi@gmail.com>
2021-05-10 11:32:56 -0300

    Test fix TypeError

--- pages/api/utils/api.ts
+++ pages/api/utils/api.ts
@@ -4,7 +4,7 @@ const api = axios.create({
   headers: {
     "Content-Type": "application/json",
     Accept: "*/*",
-    "X-Authorization": "sk_test_253819e270b95cdf86db638032063655b85138227deab", //process.env.COMMERCEJS_SK_TEST,
+    "X-Authorization": process.env.COMMERCEJS_SK_TEST,
   },
   baseURL: "https://api.chec.io/v1",
 });
